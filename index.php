619c501e258dc2c83bd495055f65b89b55be0a50
Mahrukhkhann <mahrukh.khan.official@gmail.com>
2026-07-16 18:26:00 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+$insert=false;
+if(isset($_POST['name'])){
+    
+$server="localhost";
+$user="root";
+$password="";
+
+$con=mysqli_connect($server,$user,$password);
+
+if(!$con){
+    die("connection to this database faild due to".mysqli_connect_error());
+}
+// echo"successful connection";
+
+$name =$_POST['name'];
+$age=$_POST['age'];
+$gender =$_POST['gender'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$desc =$_POST['desc']; 
+$sql= " INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `desc`, `dt`) VALUES ('$name', '$age', '$gender', '$email','$phone','$desc', current_timestamp());";
+// echo $sql;
+
+if($con->query($sql)==true){
+    // echo"Successfully inserted";
+    $insert=true;
+}else{
+    echo"error:$sql<br> $con->error";
+}
+$con->close();
+}
+?>  
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Welcome To Travel Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="bg" src="https://tse3.mm.bing.net/th/id/OIP.OtnxgXlAsLih7zWpX8tJawEyDM?r=0&rs=1&pid=ImgDetMain&o=7&rm=3
+    " alt="mountains">
+<div class="container">
+        <h1>Welcome to Trip Form</h1>
+        <p>Enter your details and submit this form to conform your participation in the trip.</p>
+        <?php
+        if($insert ==true){
+       echo" <p class='submsg'>Thanks for submitting your form. We are happy to see you joinig with us</p>";
+        }
+    ?>
+    <form action="index.php" method="post">
+
+        <input type="text" name="name" id ="name" placeholder="Enter your name">
+        <input type="text" name="age" id ="age" placeholder="Enter your age">
+        <input type="text" name="gender" id ="gender" placeholder="Enter your gender">
+        <input type="email" name="email" id="email" placeholder="Enter your email">
+        <input type="phone" name="phone" id="phone" placeholder="Enter your phone number">
+        <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+        <button class="btn">Submit</button>
+    </form>
+</div> 
+
+<script src="index.js"></script>
+</body> 
+</html>
